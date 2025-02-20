165600997bc2f6a0cff4d1dfcdc62c0995f55045
Acquaah-Emmanuel-Kofi <emmanuelacquaah2468@gmail.com>
2025-02-20 18:41:00 +0000

    feat: render components in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { NavigationEnd } from '@angular/router';
+import { TutorialService } from './tutorial/services/tutorial.service';
+import { Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [TutorialComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'test-application';
+  constructor(
+    private router: Router,
+    private tutorialService: TutorialService
+  ) {
+    router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        // Check if tutorial should be shown for new route
+        const shouldShow = this.tutorialService.shouldShowTutorialForRoute(
+          this.router.url
+        );
+        
+        if (shouldShow) {
+          this.tutorialService.startTutorial();
+        }
+      });
+  }
 }
